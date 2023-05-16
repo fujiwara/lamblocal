c880b384794803715266ce25cae54c93f99892ff
fujiwara <fujiwara.shunichiro@gmail.com>
2023-05-16 11:48:29 +0900

    change handler interface

--- lamblocal.go
+++ lamblocal.go
@@ -15,11 +15,13 @@ import (
 var Logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
 
 // Run runs a lambda hander func detect the environment (lambda or not) and run it.
-func Run[T any](ctx context.Context, fn func(context.Context, T) error) {
+func Run[T any, U any](ctx context.Context, fn func(context.Context, T) (U, error)) {
 	if strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") || os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
 		lambda.Start(fn)
 	} else {
-		if err := RunCLI(ctx, os.Stdin, fn); err != nil {
+		out, err := RunCLI(ctx, os.Stdin, fn)
+		fmt.Print(out)
+		if err != nil {
 			Logger.Error(err.Error())
 			os.Exit(1)
 		}
@@ -27,10 +29,10 @@ func Run[T any](ctx context.Context, fn func(context.Context, T) error) {
 }
 
 // RunCLI is a helper function for running a lambda hander func on CLI.
-func RunCLI[T any](ctx context.Context, src io.Reader, fn func(context.Context, T) error) error {
+func RunCLI[T any, U any](ctx context.Context, src io.Reader, fn func(context.Context, T) (U, error)) (U, error) {
 	payload := new(T)
 	if err := json.NewDecoder(src).Decode(payload); err != nil {
-		return fmt.Errorf("failed to decode payload: %w", err)
+		return *new(U), fmt.Errorf("failed to decode payload: %w", err)
 	}
 	return fn(ctx, *payload)
 }
--- lamblocal_test.go
+++ lamblocal_test.go
@@ -12,23 +12,25 @@ import (
 )
 
 func TestRunCLIOK(t *testing.T) {
-	handler := func(ctx context.Context, payload events.CloudWatchEvent) error {
+	handler := func(ctx context.Context, payload events.CloudWatchEvent) (string, error) {
 		lamblocal.Logger.Info("hello", slog.String("ID", payload.ID))
-		return nil
+		return "OK", nil
 	}
 	src := strings.NewReader(`{"id":"c4d2be6d-7987-411c-ada8-2713c427a115"}`)
-	if err := lamblocal.RunCLI(context.Background(), src, handler); err != nil {
+	if out, err := lamblocal.RunCLI(context.Background(), src, handler); err != nil {
 		t.Fatal(err)
+	} else {
+		t.Log(out)
 	}
 }
 
 func TestRunCLIError(t *testing.T) {
-	handler := func(ctx context.Context, payload events.CloudWatchEvent) error {
+	handler := func(ctx context.Context, payload events.CloudWatchEvent) (string, error) {
 		lamblocal.Logger.Info("hello", slog.String("ID", payload.ID))
-		return fmt.Errorf("error event: %s", payload.ID)
+		return "", fmt.Errorf("error event: %s", payload.ID)
 	}
 	src := strings.NewReader(`{"id":"dddccaaf-7d6e-4332-b072-f46e7ad4ee2b"}`)
-	if err := lamblocal.RunCLI(context.Background(), src, handler); err == nil {
+	if _, err := lamblocal.RunCLI(context.Background(), src, handler); err == nil {
 		t.Error("error expected")
 	} else if !strings.HasPrefix(err.Error(), "error event:") {
 		t.Error("unexpected error:", err)
